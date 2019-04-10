8bc7d73e5591164169878a5a8a0e754131d1b994
CatalinaSorina <gherzan.catalina@yahoo.com>
2019-04-10 23:02:33 +0300

    update TodoList component

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -3,7 +3,13 @@ import React from 'react';
 import Todo from './components/Todo';
 
 const TodoList = props => {
-
+    return (
+        <div className='todos'>
+            {props.todos.map(todo=>{
+                <Todo key={todo.id} todo={todo} checkTodo={props.checkTodo}/>
+            })}
+        </div>
+    )
 }
 
 export default TodoList;
