341ab81e0c4c112e318f104ddb72bcd1863a475e
CatalinaSorina <gherzan.catalina@yahoo.com>
2019-04-10 22:41:06 +0300

    fill todos array

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -1,10 +1,38 @@
 import React from 'react';
 
+const todos = [
+    {
+        id:1,
+        task:'Brush teeth',
+        finished: false
+    },
+    {
+        id:2,
+        task:'Take a shower',
+        finished: false
+    },
+    {
+        id:3,
+        task:'Cook dinner',
+        finished: false
+    },
+    {
+        id:4,
+        task:'Learn more coding',
+        finished: false
+    },
+    {
+        id:5,
+        task:'Eat breakfast',
+        finished: false
+    }
+];
+
 class TodoApp extends React.Component {
     constructor() {
         super();
         this.state = {
-            todos:[]
+            todos
         }
     }
 
